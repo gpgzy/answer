9cb38b658ce2dffe9de36bb7744cb8b5ed51a30e
gzy <786133379@qq.com>
2020-04-14 11:32:12 +0800

    init

--- src/main/java/com/example/answer/AnswerApplication.java
+++ src/main/java/com/example/answer/AnswerApplication.java
@@ -2,16 +2,26 @@ package com.example.answer;
 
 import com.example.answer.repository.impl.BaseRepositoryImpl;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories(repositoryBaseClass = BaseRepositoryImpl.class)
-@ServletComponentScan
-public class AnswerApplication {
-
-    public static void main(String[] args) {
+public class AnswerApplication extends SpringBootServletInitializer {
+    /**
+     * 重写configure方法，加载启动类
+     * @param application
+     * @return
+     */
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(AnswerApplication.class);
+    }
+    public static void main(String[] args)  {
         SpringApplication.run(AnswerApplication.class, args);
     }
 
